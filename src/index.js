bb6c97fe9c15910c33bdfc3b7ead94ba2e2681ab
Megan Fago <meganlaynefago@gmail.com>
2019-06-28 18:09:17 -0400

    one comp part 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 
 const App = () => {
     return (
-        <div>
-            <h3>Hi there!</h3>
+        <div className='ui container comments'>
+            <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img alt='avatar' src={faker.image.avatar()}/>
+                </a>
+            </div>
+            <div className='content'>
+                <a href='/' className='author'>
+                    Sam
+                </a>
+                <div className='metadata'>
+                    <span className='date'>Today at 6:00 PM</span>
+                </div>
+                <div className='text'>
+                  Nice blog post!  
+                </div>
+             </div>
         </div>
     )
 
